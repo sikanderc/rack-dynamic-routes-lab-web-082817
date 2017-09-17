aaf9540541597587775313a30736c54002685cd3
sikanderc <sikanderchowhan@gmail.com>
2017-09-17 13:34:11 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,33 @@
+class Application
+
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      if item = @@items.find{|i| i.name == item_name}
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+
+  # def handle_search(search_term)
+  #   if @@items.include?(search_term)
+  #     return "#{search_term}"
+  #   else
+  #     return "Couldn't find #{search_term}"
+  #   end
+  # end
+
+end
